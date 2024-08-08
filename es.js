77d93ce880d707da083ec5a63a8a62358a7c3f2a
hitdobariya <hitdobariya001@gmail.com>
2024-08-08 10:01:31 +0530

    08-08-24

--- es.js
+++ es.js
@@ -1,71 +1,26 @@
-// const express = require('express');
+const express = require('express');
 
-// const data = [
-//     {id: 1, name: 'John Doe', age: 25},
-//     {id: 2, name: 'Jane Doe', age: 30},
-//     {id: 3, name: 'Alice Doe', age: 28}
-//  ];
-
-// const server = express();
-
-// server.get('/',(req, res) => {
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.write('Hello, world');
-//     res.end();
-// });
-
-// server.get('./about',(req, res) => {
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.write('This is about page');
-//     res.end();
-// });
-
-// server.get('/api/users',(req, res) => {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.json(data);
-// });
-
-// server.listen(1000,()=>{
-//     console.log(`Server is running on port http://localhost:1000`);  
-// });
+const server = express();
 
 
+//built-in middleware
+
+server.use(express.json());
+// server.use(express.urlencoded({extended: true}));
+
+const middleWare = (req,res,next)=>{
+    console.log(req.body.name);
+    next();
+};
 
 
-// const express = require('express');
-
-// const server = express();
-
-// server.get('/',(req, res) => {
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.send('Hello, world');
-//     res.end();
-// });
-
-// server.post('/',(req, res) => {
-//     res.setHeader('Content-type', 'text/plain');
-//     res.send('post method');
-//     res.end();
-// });
-
-// server.patch('/',(req, res) => {
-//     res.setHeader('Content-type', 'text/plain');
-//     res.send('patch method');
-//     res.end();
-// });
-
-// server.put('/',(req, res) => {
-//     res.json({method : 'put method'});
-// });
-
-// server.delete('/',(req,res) => {
-//     res.setHeader('Content-type', 'text/plain');
-//     res.send('delete method');
-// });
-
-// server.listen(2000,()=>{
-//     console.log(`Server is running on port http://localhost:2000`);  
-// });
+server.get('/',(req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello, world');
+    res.end();
+});
 
 
-
+server.listen(4000,()=>{
+    console.log(`Server is running on port http://localhost:4000`);  
+});
