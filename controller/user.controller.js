f9d18497e630091a885366ff53c202e91b163dc5
hitdobariya <hitdobariya001@gmail.com>
2024-08-14 08:56:31 +0530

    13-08-24

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,5 @@
 const User = require('../model/user.model');
 
-exports.getUser = async (req, res) => {
-    try {
-        res.json(User);
-    } catch {
-        res.status(500);
-    }
-};
-
 exports.addUser = async (req, res) => {
     try{
     let user = await User.create({...req.body});
@@ -17,31 +9,3 @@ exports.addUser = async (req, res) => {
         res.status(500);
     }
 };
-
-// exports.replaceUser = (req, res) => {
-//     let id = +req.params.id;
-//     let userIndex = user.findIndex((user) => user.id === id);
-//     user.splice(userIndex, 1, { ...req.body });
-//     res.json({ message: 'user replace successfully' });
-// };
-
-// exports.updateUser = (req, res) => {
-//     let id = +req.params.id;
-//     let userIndex = user.findIndex((user) => user.id === id);
-//     let users = user[userIndex];
-//     user.splice(userIndex, 1, { ...users, ...req.body });
-//     res.json({ message: 'user update successfully' });
-// };
-
-// exports.deleteUser = (req, res) => {
-//     let id = +req.params.id;
-//     let userIndex = user.findIndex((user) => user.id === id);
-//     user.splice(userIndex, 1);
-//     res.json({ message: 'user delete successfully' });
-// };
-
-// exports.getSingleUser = (req, res) => {
-//     let id = +req.params.id;
-//     let users = user.find((user) => user.id === id);
-//     res.json(users);
-// };
