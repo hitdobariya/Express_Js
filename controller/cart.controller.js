8ef158d94617ed896f4ca7637ff98d2bb815ce69
hitdobariya <hitdobariya001@gmail.com>
2024-08-30 09:57:32 +0530

    30-08-24

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -5,7 +5,7 @@ exports.addToCart = async (req, res) => {
         let userId = req.user._id;
         let cart = await Cart.findOne({ user: userId, productId: req.body.productId, isDelete: false });
         if (cart) {
-            return res.json({ message: 'alredy exist...' });
+            return await Cart.findByIdAndUpdate({ cartId: cart.cartId }, { $inc: { quantity: +1 } }, { new: true });
         }
         cart = await Cart.create({ user: userId, ...req.body });
         res.status(201).json({ message: 'cart added...', cart })
@@ -16,6 +16,30 @@ exports.addToCart = async (req, res) => {
 };
 
 exports.getAllCarts = async (req, res) => {
-    let carts = await Cart.find({ user: req.user._id, isDelete: false });
+    let carts = await Cart.find({ user: req.user._id });
     res.json(carts);
-}
+};
+
+exports.deleteCart = async (req, res) => {
+    try {
+        let cart = await Cart.updateOne({ _id: req.body.cartId }, { $set: { isDelete: true } }, { new: true });
+        console.log(cart);
+        if (!cart) return res.status(404).json({ message: 'cart not found...' });
+        res.status(200).json({ message: 'cart deleted...', cart });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error...' });
+    }
+};
+
+exports.updateCart = async (req, res) => {
+    try {
+        let cart = await Cart.updateOne({ _id: req.query.cartId }, { $set: { quantity: req.query.quantity } }, { new: true });
+        console.log(cart);
+        if (!cart) return res.status(404).json({ message: 'cart not found...' });
+        res.status(200).json({ message: 'cart updated...', cart });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error...' });
+    }
+};
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cartRoutes = express.Router();
-const {addToCart, getAllCarts} = require('../controller/cart.controller');
+const { 
+    addToCart,
+    getAllCarts,
+    deleteCart,
+    updateCart
+} = require('../controller/cart.controller');
 const { verifyToken } = require("../helper/tokenVerify");
 
-cartRoutes.post('/addtocart', verifyToken, addToCart );
-cartRoutes.get('/getcarts', verifyToken,  getAllCarts );
+cartRoutes.post('/addtocart', verifyToken, addToCart);
+cartRoutes.get('/getcarts', verifyToken, getAllCarts);
+cartRoutes.delete('/deletecart', verifyToken, deleteCart);
+cartRoutes.patch('/update',verifyToken, updateCart);
 
 module.exports = cartRoutes;
